1c9a6d9259a57cbbd9d2bfc108211d6fe600ea56
Farijul Tanjim <67682096+farijultanjim@users.noreply.github.com>
2023-12-04 14:06:15 +0600

    mapping revers

--- components/GlobalImages.jsx
+++ components/GlobalImages.jsx
@@ -5,7 +5,7 @@ const GlobalImages = ({ data }) => {
   return (
     <div>
       <div className="space-y-6 py-8 sm:columns-2 sm:gap-6 xl:columns-3">
-        {data.slice().reverse().map((post) => (
+        {data.map((post) => (
           <ImageCard key={post.id} post={post} />
         ))}
       </div>
